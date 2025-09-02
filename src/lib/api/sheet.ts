d5697a26ca4a6b86ee098c7533ae5bfe5f520b62
JosephTseng <zxp930110@hotmail.com.tw>
2025-09-02 15:33:14 +0800

    set token

--- src/lib/api/sheet.ts
+++ src/lib/api/sheet.ts
@@ -1,33 +1,33 @@
-import type { WordItem, WordListResponse } from "$lib/types";
-import { PUBLIC_APP_SCRIPT_URL } from "$env/static/public";
-import { mockWordItems } from "./mock";
+import type { UpdateFields, WordItem, WordListResponse } from '$lib/types';
+import { PUBLIC_APP_SCRIPT_URL, PUBLIC_APP_SCRIPT_TOKEN } from '$env/static/public';
+import { mockWordItems } from './mock';
 
 const ENDPOINT = PUBLIC_APP_SCRIPT_URL; // .env 讀
 
 export async function getMockWordItems(): Promise<WordItem[]> {
-  // 模擬從伺服器取得資料
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(mockWordItems);
-    }, 500);
-  });
+	// 模擬從伺服器取得資料
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(mockWordItems);
+		}, 1000);
+	});
 }
 
 export async function getWordList(): Promise<WordItem[]> {
-  // const res = await fetch(`${ENDPOINT}?action=getList`);
-  // const data = (await res.json()) as WordListResponse;
-  const data = await getMockWordItems().then((items) => ({
-    ok: true,
-    result: items,
-  }));
+	// const res = await fetch(`${ENDPOINT}?action=getList`);
+	// const data = (await res.json()) as WordListResponse;
+	const data = await getMockWordItems().then((items) => ({
+		ok: true,
+		result: items,
+	}));
 
-  if (!data.ok) {
-    throw new Error("listDue failed");
-  }
+	if (!data.ok) {
+		throw new Error('getWordList failed');
+	}
 
-  console.log("Fetched word list:", data);
+	console.log('Fetched word list:', data);
 
-  return data.result;
+	return data.result;
 }
 
 // export async function appendWord(payload: Partial<WordItem> & { word:string; meaning:string; reviewStage?: number }) {
@@ -39,11 +39,15 @@ export async function getWordList(): Promise<WordItem[]> {
 //   return res.json();
 // }
 
-export async function updateReview(id: string, correct: boolean) {
-  const res = await fetch(ENDPOINT, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ op: "updateReview", id, correct }),
-  });
-  return res.json();
+export async function updateReview(updateFields: UpdateFields) {
+	const res = await fetch(ENDPOINT, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			op: 'updateReview',
+			token: PUBLIC_APP_SCRIPT_TOKEN,
+			fields: updateFields,
+		}),
+	});
+	return res.json();
 }
