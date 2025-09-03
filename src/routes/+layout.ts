83ca4b981a2875cc683c64282628340fe64fcaf7
JosephT5566 <30860126+JosephT5566@users.noreply.github.com>
2025-09-03 22:04:45 +0800

    typo

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,7 @@ import type { LayoutLoad } from './$types';
 import { browser } from '$app/environment';
 import { redirect } from '@sveltejs/kit';
 import { getIsSignedIn } from '$lib/auth';
-import { base } from '$app/path'; // using the paths.base set in svelte.config.js
+import { base } from '$app/paths'; // using the paths.base set in svelte.config.js
 
 export const load: LayoutLoad = async ({ url }) => {
     // 僅在瀏覽器端檢查（localStorage 僅存在於瀏覽器）
