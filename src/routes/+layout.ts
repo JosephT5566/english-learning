a3f3931f7a95b984ad74bc4cecb757daf2e4b6b1
JosephTseng <zxp930110@hotmail.com.tw>
2025-09-06 18:55:23 +0800

    Change the Google sign in process

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,13 +2,18 @@ import type { LayoutLoad } from './$types';
 import { browser } from '$app/environment';
 import { redirect } from '@sveltejs/kit';
 import { getIsSignedIn } from '$lib/auth';
+import { setSignIn } from '$lib/stores/auth';
 import { base } from '$app/paths'; // using the paths.base set in svelte.config.js
 
 export const load: LayoutLoad = async ({ url }) => {
     // 僅在瀏覽器端檢查（localStorage 僅存在於瀏覽器）
     if (browser) {
         const isHome = url.pathname === `${base}/`;
-        if (!isHome && !getIsSignedIn()) {
+        const isLocalStorageSignedIn = getIsSignedIn();
+        if (isLocalStorageSignedIn) {
+            setSignIn();
+        }
+        if (!isHome && !isLocalStorageSignedIn) {
             throw redirect(307, `${base}/`);
         }
     }
